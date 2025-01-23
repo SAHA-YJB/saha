8e63c6dce7c439b4f6549ff6a64b78910766cebf
SAHA-YJB <pkomom123@naver.com>
2025-01-23 11:54:48 +0900

    사하마켓 init

---
+++ src/components/common/BottomNav.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Home, MessageCircle, User } from 'lucide-react';
+
+const navItems = [
+  {
+    icon: Home,
+    label: '홈',
+    path: '/',
+  },
+  {
+    icon: MessageCircle,
+    label: '채팅',
+    path: '/chat',
+  },
+  {
+    icon: User,
+    label: '마이',
+    path: '/mypage',
+  },
+];
+
+export default function BottomNav() {
+  const pathname = usePathname();
+
+  return (
+    <div className='btm-nav bg-base-100 border-t'>
+      {navItems.map((item) => (
+        <Link
+          key={item.path}
+          href={item.path}
+          className={`${
+            pathname === item.path ? 'active text-primary' : 'text-neutral'
+          }`}
+        >
+          <item.icon className='w-5 h-5' />
+          <div className='btm-nav-label'>{item.label}</div>
+        </Link>
+      ))}
+    </div>
+  );
+}
---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+import BottomNav from './BottomNav';
+
+export const Footer = () => {
+  return (
+    <footer className='fixed bottom-0 left-0 right-0'>
+      <BottomNav />
+    </footer>
+  );
+};
